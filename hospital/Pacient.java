518d5d8c59087ec54fe2ef8bdb982e013080fa7b
Alberto <41839976+bertbertbertbert@users.noreply.github.com>
2026-02-06 17:08:38 +0100

    proyectoPersonal

--- hospital/Pacient.java
+++ hospital/Pacient.java
@@ -6,4 +6,30 @@ double diners;
 int edat;
 Gravetat gravetat;
 Planta planta;
+
+public Pacient(String nom, double diners, int edat, Gravetat gravetat) {
+	this.nom = nom; this.diners = diners;this.gravetat = gravetat;
+	
+	if(edat	< 0) {
+		this.edat = 0;
+	}else if(edat > 150){
+		this.edat = 150;
+	}else {
+		this.edat = edat;
+	}
+	
+	if(edat <= 1) {
+	  this.planta = Planta.NEONATAL;
+	}else if(edat>1 && edat<=18) {
+		  this.planta = Planta.PEDIATRIA;		
+	}else if(edat>18 && edat<=74) {
+		  this.planta = Planta.GENERAL;		
+	}else {
+		  this.planta = Planta.GERIATRIA;			
+	}
+}
+
+public void diguesNom() {
+	System.out.println(this.nom);
+}
 }
